628e2fe2ce005202500adbd07622060c4f248045
lereden <n1243456799761@gmail.com>
2021-11-15 14:12:39 +0800

    add unionfind example code

---
+++ unionfind.py
@@ -0,0 +1,12 @@
+class UnionFind():
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+    def union(self, a, b):
+        if self.find(a) != self.find(b):
+            self.parent[self.parent[b]] = self.parent[a]
+    def find(self, a):
+        if self.parent[a] == a:
+            return a
+        else:
+            self.parent[a] = self.find(self.parent[a])
+            return self.parent[a]
