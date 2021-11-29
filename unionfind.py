a9a3bf42c79e79c1e06bdaa0ec7b899018dbce45
lereden <n1243456799761@gmail.com>
2021-11-29 10:08:54 +0800

    add diction based UF

--- unionfind.py
+++ unionfind.py
@@ -10,3 +10,16 @@ class UnionFind():
         else:
             self.parent[a] = self.find(self.parent[a])
             return self.parent[a]
+
+class UnionFind():
+    def __init__(self):
+        self.parent = {}
+    def union(self, a, b):
+        if self.find(a) != self.find(b):
+            self.parent[self.parent[b]] = self.parent[a]
+    def find(self, a):
+        if a not in self.parent:
+            self.parent[a] = a
+        if self.parent[a] != a:
+            self.parent[a] = self.find(self.parent[a])
+        return self.parent[a]
