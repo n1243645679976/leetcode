1f0cd3a905f40b32f5c0415b44a7cfa34efb7829
lereden <n1243456799761@gmail.com>
2021-12-07 09:50:41 +0800

    add solution to prob 1290

---
+++ 1290/v1.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def getDecimalValue(self, head):
+        """
+        :type head: ListNode
+        :rtype: int
+        """
+        ans = 0
+        node = head
+        while node:
+            ans *= 2
+            ans += node.val
+            node = node.next
+        return ans
