b76334629c2cd285a25493edc6fbad55d58feb8e
lereden <n1243456799761@gmail.com>
2021-12-21 16:42:16 +0800

    add solution to prob v1

---
+++ 231/v1.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0: return False
+        while n != 1:
+            # print(n, n&1)
+            if n & 1: return False
+            n >>= 1
+        return True
