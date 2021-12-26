d088cda832e634ef1311adabc14271ac5fa87bdd
lereden <n1243456799761@gmail.com>
2021-12-26 11:37:03 +0800

    add oneline solution to prob 973

---
+++ 973/oneline.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return sorted(points, key=lambda x:x[0]*x[0]+x[1]*x[1])[:k]
